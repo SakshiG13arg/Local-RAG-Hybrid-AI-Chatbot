87cdd6be7e5417b4ede2bf604690773129fe1f86
SakshiG13arg <sakshigarg0913@gmail.com>
2026-07-28 21:06:15 +0530

    Update chunk_document import path in pipeline.py

--- ingestion/pipeline.py
+++ ingestion/pipeline.py
@@ -1,6 +1,6 @@
 from ingestion.loader import load_pdf
 from ingestion.cleaner import clean_text
-from chunking.router import chunk_document
+from chunking.router_chunking import chunk_document
 from ingestion.embedder import create_embeddings
 
 from utils.vector_store import create_vector_store
